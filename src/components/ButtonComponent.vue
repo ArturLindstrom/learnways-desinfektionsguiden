c89f80baa9602796c49ba278c41a0754127f90dd
axelcatasus <axel.catasus@gmail.com>
2022-08-18 10:47:23 +0200

    kk slider

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,11 +1,35 @@
 <template>
-  <button>
+  <button @click="action">
     <slot />
   </button>
 </template>
 
 <script setup>
+import {useRoute, useRouter} from 'vue-router';
+import { defineProps, computed } from 'vue';
 
+const props = defineProps({
+  action: {
+    type: String,
+    default: 'forward',
+  },
+})
+
+const route = useRoute();
+const router = useRouter();
+const routes = ["home", "olika-typer", "situationer", "diplom" ]
+const forward = () => {
+  const index = routes.indexOf(route.name);
+  if (index < routes.length - 1) {
+    console.log(props.action)
+    router.push(routes[index + 1]);
+  }
+}
+const action = () => {
+  if (props.action === 'forward') {
+    forward();
+  }
+}
 </script>
 
 <style scoped lang="scss">
