004e1b28389ddfffb1c033bc05fa567b8f855612
arturlindstrom <artur.lindstrom@gmail.com>
2023-12-19 13:16:20 +0100

    deploy test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 import { Vue3Mq } from "vue3-mq";
 
-createApp(App).use(store).use(router).use(Vue3Mq).mount('#app')
-
+createApp(App).use(store).use(router).use(Vue3Mq).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import OlikaTyperView from '../views/OlikaTyperView.vue'
-import SituationerView from '../views/SituationerView.vue' 
-import DiplomView from '../views/DiplomView.vue'
-import store from '../store/index.js'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import OlikaTyperView from "../views/OlikaTyperView.vue";
+import SituationerView from "../views/SituationerView.vue";
+import DiplomView from "../views/DiplomView.vue";
+import store from "../store/index.js";
 
 const areViewsCompleted = () => {
-  if(store.getters.viewsCompleted) {
-    store.commit('completedRoute', 'diplom')
-    store.commit('toggleDone')
+  if (store.getters.viewsCompleted) {
+    store.commit("completedRoute", "diplom");
+    store.commit("toggleDone");
   }
-  return true
-}
+  return true;
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-          path: '/',
-          name: 'home',
-          component: HomeView,
-          meta: {title: 'Start'}
-        },
-        {
-          path: '/olika-typer',
-          name: 'olikatyper',
-          component: OlikaTyperView,
-          meta: {title: 'Olika typer'}
-        },
-        {
-          path: '/situationer',
-          name: 'situationer',
-          component: SituationerView,
-          meta: {title: 'Situationer'}
-        },
-        {
-          path: '/diplom',
-          name: 'diplom',
-          component: DiplomView,
-          meta: {title: 'Diplom'},
-          beforeEnter: areViewsCompleted
-        }
-    
+      path: "/",
+      name: "home",
+      component: HomeView,
+      meta: { title: "Start" },
+    },
+    {
+      path: "/olika-typer",
+      name: "olikatyper",
+      component: OlikaTyperView,
+      meta: { title: "Olika typer" },
+    },
+    {
+      path: "/situationer",
+      name: "situationer",
+      component: SituationerView,
+      meta: { title: "Situationer" },
+    },
+    {
+      path: "/diplom",
+      name: "diplom",
+      component: DiplomView,
+      meta: { title: "Diplom" },
+      beforeEnter: areViewsCompleted,
+    },
   ],
   scrollBehavior(to, from, savedPosition) {
-    // always scroll to top
-    return { top: 0, behavior: 'smooth' }
-  }
-
-})
-
-router.afterEach((to, from) => {
-  document.title = to.meta.title + ' | Desinfektionsguiden';
+    return { top: 0, behavior: "smooth" };
+  },
 });
 
+router.afterEach((to, from) => {
+  document.title = to.meta.title + " | Desinfektionsguiden";
+});
 
-export default router
+export default router;
